0d2de73004f1c9105aa98eb14c8fe6785bf3573f
sudhir <strr7452@gmail.com>
2019-04-01 01:05:02 +0530

    Updates

---
+++ precache-manifest.1dbc8a3c46e9f09c0d550ff3bb7ccf51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clock/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1d811e2acbdbe64bfd52",
+    "url": "/clock/static/js/runtime~main.4586ffc4.js"
+  },
+  {
+    "revision": "999d1b479bee0b8bf9e1",
+    "url": "/clock/static/js/main.3dac9a11.chunk.js"
+  },
+  {
+    "revision": "30616f8a1abe224c0e35",
+    "url": "/clock/static/js/2.a0c5e020.chunk.js"
+  },
+  {
+    "revision": "999d1b479bee0b8bf9e1",
+    "url": "/clock/static/css/main.3b5984f3.chunk.css"
+  },
+  {
+    "revision": "3c69525b1625d75d8e9de3be58d31489",
+    "url": "/clock/index.html"
+  }
+];
