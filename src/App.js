72b610b9f887805e55af1311b8b62a597ef78504
sudhir <strr7452@gmail.com>
2019-04-01 01:02:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MIDISounds from 'midi-sounds-react';
+const App = () => {
 
-class App extends Component {
-  playTestInstrument() {
-    this.midiSounds.playChordNow(3, [70], 2.5, 6, 9);
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to midi-sounds-react example 1</h1>
-        </header>
-        <p className="App-intro">Press Play to play instrument sound.</p>
-        <p><button onClick={this.playTestInstrument.bind(this)}>Play</button></p>
-        <MIDISounds ref={(ref) => (this.midiSounds = ref)} appElementName="root" instruments={[3]} />
-      </div>
-    );
-  }
+  const [date, setDate] = useState(new Date());
+  const [sec, setSec] = useState(date.getSeconds())
+  useEffect(() => {
+    //  angle h = 0.5 *(60*h + m)
+    // angle m = 6 * m 
+    let interval = setInterval(() => {
+      setDate(new Date())
+    }, 1000);
+
+
+    (function start() {
+      var hour = document.getElementById("hour")
+      hour.setAttribute("transform", "rotate(" + (90 + 0.5 * (60 * (date.getHours() >= 12 ? date.getHours() - 12 : date.getHours()) + date.getMinutes())) + ",200,150)")
+      var min = document.getElementById("min")
+      min.setAttribute("transform", "rotate(" + (90 + (6 * date.getMinutes())) + ",200,150)")
+      var sec = document.getElementById("sec")
+      sec.setAttribute("transform", "rotate(" + (6 * date.getSeconds() + 90) + ",200,150)")
+    })();
+    return () => clearInterval(interval)
+  }, [sec, date]);
+
+  return (
+    <> 
+      <h3>{date.toString()}</h3>
+      {/*
+      <p>Hour : {date.getHours()}</p>
+      <p>minutes : {date.getMinutes()}</p>
+      <p>second : {date.getSeconds()}</p>
+      <p>angle h:{0.5 * (60 * (date.getHours() >= 12 ? date.getHours() - 12 : date.getHours()) + date.getMinutes())} </p>
+      <p>angle min :{6 * date.getMinutes()} </p>
+      <p>angle sec :{6 * date.getSeconds()} </p>
+
+      */}
+      <svg height="500" width="500" id="clock">
+        <circle cx="200" cy="150" r="120" stroke="black" strokeWidth={1 || date.getSeconds()} fill="#fff" />
+        <line id="hour" x1="200" y1="150" x2="120" y2="150" transform="rotate(100,200,150)" style={{ fill: 'blue', stroke: 'pink', strokeWidth: '10', opacity: '0.9' }} />
+        <line id="min" x1="200" y1="150" x2="88" y2="150" transform="rotate(100,200,150)" style={{ fill: 'blue', stroke: 'red', strokeWidth: '8', opacity: '0.7' }} />
+        <line id="sec" x1="200" y1="150" x2="78" y2="150" transform="rotate(100,200,150)" style={{ fill: 'blue', stroke: 'blue', strokeWidth: '5', opacity: '0.5' }} />
+        <circle cx="200" cy="150" r="10" strokeWidth={1 || date.getSeconds()} fill="black" />
+
+      </svg>
+
+    </>
+  )
 }
 
 export default App;
