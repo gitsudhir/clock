69eecee071fc134db811a7eae28fe099153fd8b0
sudhir <strr7452@gmail.com>
2019-04-24 21:50:40 +0530

    hour number

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const App = () => {
 
   const [date, setDate] = useState(new Date());
   const [sec, setSec] = useState(date.getSeconds())
+  const [hourDraw, setHourDraw] = useState([]);
+
   useEffect(() => {
     //  angle h = 0.5 *(60*h + m)
     // angle m = 6 * m 
@@ -15,17 +17,31 @@ const App = () => {
 
     (function start() {
       var hour = document.getElementById("hour")
-      hour.setAttribute("transform", "rotate(" + (90 + 0.5 * (60 * (date.getHours() >= 12 ? date.getHours() - 12 : date.getHours()) + date.getMinutes())) + ",200,150)")
+      //console.log(hour.getAttribute("x1"));
+      let hourAngle = (90 + 0.5 * (60 * (date.getHours() >= 12 ? date.getHours() - 12 : date.getHours()) + date.getMinutes()));
+
+      hour.setAttribute("transform", "rotate(" + hourAngle + ",200,150)")
       var min = document.getElementById("min")
+
       min.setAttribute("transform", "rotate(" + (90 + (6 * date.getMinutes())) + ",200,150)")
       var sec = document.getElementById("sec")
       sec.setAttribute("transform", "rotate(" + (6 * date.getSeconds() + 90) + ",200,150)")
+      var hourList = [];
+      for (let i = 1; i <= 12; i++) {
+        const xx = (200 + 133 * Math.cos(30 * i * Math.PI / 180));
+        const yy = (150 + 133 * Math.sin(30 * i * Math.PI / 180));
+        hourList.push({ x: xx, y: yy })
+      }
+   
+    
+      // console.log('ga',hourList)
+      setHourDraw(hourList);
     })();
     return () => clearInterval(interval)
   }, [sec, date]);
 
   return (
-    <> 
+    <>
       <h3>{date.toString()}</h3>
       {/*
       <p>Hour : {date.getHours()}</p>
@@ -36,17 +52,32 @@ const App = () => {
       <p>angle sec :{6 * date.getSeconds()} </p>
 
       */}
-      <svg height="500" width="500" id="clock">
-        <circle cx="200" cy="150" r="120" stroke="black" strokeWidth={1 || date.getSeconds()} fill="#fff" />
+      <svg height="520" width="520" id="clock">
+        <defs>
+          <radialGradient id="grad1" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+            <stop offset="0%" style={{ stopColor: 'rgb(255, 255, 255)', stopOpacity: 0 }} />
+            <stop offset="100%" style={{ stopColor: 'rgb(99, 122, 255)', stopOpacity: 1 }} />
+          </radialGradient>
+        </defs>
+        <circle cx="200" cy="150" r="120" stroke="black" strokeWidth={1 || date.getSeconds()} fill="url(#grad1)" />
+        <circle cx="200" cy="150" r="150" stroke="black" strokeWidth={2} fill="url(#grad1)" />
         <line id="hour" x1="200" y1="150" x2="120" y2="150" transform="rotate(100,200,150)" style={{ fill: 'blue', stroke: 'pink', strokeWidth: '10', opacity: '0.9' }} />
         <line id="min" x1="200" y1="150" x2="88" y2="150" transform="rotate(100,200,150)" style={{ fill: 'blue', stroke: 'red', strokeWidth: '8', opacity: '0.7' }} />
         <line id="sec" x1="200" y1="150" x2="78" y2="150" transform="rotate(100,200,150)" style={{ fill: 'blue', stroke: 'blue', strokeWidth: '5', opacity: '0.5' }} />
         <circle cx="200" cy="150" r="10" strokeWidth={1 || date.getSeconds()} fill="black" />
-
+        {
+          hourDraw.map(({ x, y }, i) => (
+            <Text x={x} y={y} i={(i == 8 ? i + 4 : (i + 4) % 12)} />
+          ))
+        }
       </svg>
 
     </>
   )
 }
+
+const Text = (({ x, y, i }) => {
+  return <text x={x} y={y} fill="gold">{i}</text>
+})
 
 export default App;
